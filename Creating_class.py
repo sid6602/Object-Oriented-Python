fa6ab88ffb48d22bcaff719bea7d90733cb1c4cf
Siddhi Mankar <68636373+sid6602@users.noreply.github.com>
2021-11-24 23:39:09 +0530

    Creating class and objects

---
+++ Creating_class.py
@@ -0,0 +1,12 @@
+class stu:
+    pass
+
+obj1= stu()
+obj2= stu()
+
+obj1.name= "Sid"
+obj1.sub= ["Physics", "Chem", "Math"]
+obj2.name= "Mankar"
+obj2.sub= ["Physics", "Chem", "Bio"]
+
+print(obj1.name, obj1.sub, "\n", obj2.name, obj2.sub)
